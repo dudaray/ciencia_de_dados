f5f80efc232592137e9b38fa35445bee2abb4dd0
dudaray <141421272+dudaray@users.noreply.github.com>
2024-10-21 15:27:02 -0300

    s

--- gráficos/quantidadeUsuarios.js
+++ gráficos/quantidadeUsuarios.js
@@ -4,7 +4,7 @@ async function quantidadeUsuarios() {
   const dados = await res.json()
   const nomeDasRedes = Object.keys(dados)
   const quantidadeUsuarios = Object.values(dados)
-  
+
   const data = [
       {
         x: nomeDasRedes,
@@ -17,8 +17,6 @@ async function quantidadeUsuarios() {
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
   Plotly.newPlot(grafico, data)
-    
-    console.log(dados)
-}
+  }
 
 quantidadeUsuarios()
