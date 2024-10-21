14c6daceb15bc27d21cae0cee91e110d8e872ea9
dudaray <141421272+dudaray@users.noreply.github.com>
2024-10-21 11:06:29 -0300

    s

--- gráficos/quantidadeUsuarios.js
+++ gráficos/quantidadeUsuarios.js
@@ -1,1 +1,9 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+      
+      console.log(dados)
+  }
+
+  quantidadeUsuarios()
