9244d24b4d238e57f9030b5e3b568e5dcfa15a03
dudaray <141421272+dudaray@users.noreply.github.com>
2024-10-21 10:48:13 -0300

    s

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -9,3 +9,5 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
   console.log(paragrafo)
 }
+
+vizualizarInformacoesGlobais()
