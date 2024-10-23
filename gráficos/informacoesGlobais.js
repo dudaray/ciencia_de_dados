0605e112fa50afda736c9d74f768c70b9d223232
dudaray <141421272+dudaray@users.noreply.github.com>
2024-10-23 12:29:26 -0300

    s

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -12,7 +12,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo} bilhões</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     console.log(paragrafo)
     
   // código omitido
